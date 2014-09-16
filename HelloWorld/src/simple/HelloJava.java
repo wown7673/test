e285df4945b47950497d2fa9c2a2218b73e5d8ea
jaejoo <wown7673@naver.com>
2014-09-16 13:56:08 +0900

    테스트용 커밋~

---
+++ HelloWorld/src/simple/HelloJava.java
@@ -0,0 +1,12 @@
+package simple;
+
+public class HelloJava {
+	public static void main(String[] args) {
+		String str = "Hello World!!!!!";
+		print(str);
+	}
+
+	public static void print(String str) {
+		System.out.println(str+" :: ");
+	}
+}
---
+++ HelloWorld/src/simple/Math.java
@@ -0,0 +1,17 @@
+package simple;
+
+public class Math {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( add(2, 3) );
+		
+		System.out.println(add(3,5));
+	}
+
+	public static int add(int a, int b) {
+		int sum = a + b;
+		return sum;
+	}
+
+}
